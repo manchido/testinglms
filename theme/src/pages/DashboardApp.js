e7cf498285ad89d2a288b628ccfbbf12b56f41ff
tamilaxis <71686318+tamilaxis@users.noreply.github.com>
2021-10-22 21:20:21 +0530

    modfity minimal text

--- theme/src/pages/DashboardApp.js
+++ theme/src/pages/DashboardApp.js
@@ -24,7 +24,7 @@ export default function DashboardApp() {
   const navigate = useNavigate();
   if(sessionStorage.getItem('token') === '' || sessionStorage.getItem('token') === null){
   return (
-    <Page title="Dashboard | Minimal-UI">
+    <Page title="Dashboard | Nudge">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
           <Typography variant="h4">Hi, Welcome back</Typography>
